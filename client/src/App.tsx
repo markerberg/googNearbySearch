52350782f3950d7e527d8a8ff361406dde778d68
Mark Goberdhan <markgoberdhan@gmail.com>
2023-01-30 20:53:20 -0500

    transform coordinates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,55 +13,61 @@ const locations = [
   {"name": "Columbia River Gorge, OR", "coordinates": "45.7253, -121.7300"}
 ]
 
-// const PUBLIC_API_KEY = 'AIzaSyDIb6tuC5IBX5yf8pYBMs_hLkZicqDHZ9k';
+const PUBLIC_API_KEY = 'AIzaSyDIb6tuC5IBX5yf8pYBMs_hLkZicqDHZ9k';
 
 function App() {
   const [userLocation, setUserLocation] = useState('');
   const [userActivity, setUserActivity] = useState('');
-  const [userResults, setUserResults] = useState([]);
+  const [userResults, setUserResults] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [noResults, setNoResults] = useState(false);
 
-  // const transFormAddresses = (data) => {
-  //   let newData =  [];
-  //   data.forEach(async (item, i) => {
-  //     const { lat } = item.geometry.location;
-  //     const { lng } = item.geometry.location;
-  //     const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${PUBLIC_API_KEY}`
-  //     // consider caching this
-  //     const data = await fetch(url);
-  //     const json = await data.json();
+  const transFormAddresses = (data:any) => {
+    let newData: NewItem[] =  [];
+    data.forEach(async (item: Item, i: number) => {
+      const { lat } = item.geometry.location;
+      const { lng } = item.geometry.location;
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${PUBLIC_API_KEY}`
+      // consider caching this
+      const data = await fetch(url);
+      const json = await data.json();
+      const address = json.results[0].formatted_address;
       
-  //     newData.push({
-  //       "address": json.results[0].formatted_address,
-  //       "name": item.name,
-  //       "id": item.place_id,
-  //       "rating": item.rating
-  //     })
-  //   });
-  //   return newData;
-  // }
+      newData.push({
+        "address": address,
+        "name": item.name,
+        "id": item.place_id,
+        "rating": item.rating
+      })
+    });
+    return newData;
+  }
 
 
   const fetchData = async (location: string, activity: string): Promise<any> => {
     setIsLoading(true);
-    const response = await fetch(`api/${location}/${activity}`); 
+    const response = await fetch(`api/${location}/${activity}`);
     let json = await response.json();
-    // let newData = transFormAddresses(JSON.parse(json).results);
-    if(JSON.parse(json).status == "ZERO_RESULTS") {
+    if(JSON.parse(json).status === "ZERO_RESULTS") {
       setNoResults(true);
     } else {
       setNoResults(false);
     }
-    setUserResults(JSON.parse(json).results);
-    setIsLoading(false);
+    return JSON.parse(json).results;
   }
 
   useEffect(() => {
     if(userLocation && userActivity) {
-      fetchData(userLocation, userActivity);
+      fetchData(userLocation, userActivity)
+        .then(data => transFormAddresses(data))
+        .then(res => {
+          setIsLoading(false);
+          setUserLocation('')
+          setUserActivity('')
+          setUserResults(res);
+        });
     }
-  }, [userLocation, userActivity])
+  }, [userLocation, userActivity, userResults])
   
   const captureLocation = (locationVal: string) => {
     setUserLocation(locationVal);
@@ -78,14 +84,34 @@ function App() {
         <SearchBar searchHandler={captureSearchInput}/>
       </div>
       <div className="input-results">
-        {isLoading ? 
-          <p>Loading...</p>
-          :
-          <LocationList zeroResults={noResults} list={userResults} />
-        }
+        {isLoading && <p>Loading...</p>}
+        {userResults && <LocationList zeroResults={noResults} list={userResults} />}
       </div>
     </div>
   );
+}
+
+interface Location {
+  lat: number,
+  lng: number
+}
+
+interface Geometry {
+  location: Location
+}
+
+interface Item {
+  place_id: number,
+  name: string,
+  rating: number,
+  geometry: Geometry
+}
+
+interface NewItem {
+  address: number,
+  name: string,
+  rating: number,
+  id: number
 }
 
 export default App;
